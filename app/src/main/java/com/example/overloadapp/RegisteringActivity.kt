6d147ea3bcd06c130b7327ce44b48a1673535736
choeyujeong911 <yujeongchoe20@gmail.com>
2025-10-02 16:22:52 +0900

    디바이스 목록 디자인 수정

--- app/src/main/java/com/example/overloadapp/RegisteringActivity.kt
+++ app/src/main/java/com/example/overloadapp/RegisteringActivity.kt
@@ -1,82 +1,71 @@
 package com.example.overloadapp
 
-import android.content.Intent
+import android.Manifest
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.bluetooth.BluetoothManager
+import android.bluetooth.BluetoothSocket
+import android.content.BroadcastReceiver
 import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Build
-import androidx.activity.compose.ManagedActivityResultLauncher
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.content.ContextCompat
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
+
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.annotation.RequiresPermission
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.example.overloadapp.ui.theme.MyBlue40
-import com.example.overloadapp.ui.theme.MyBlue80
-import com.example.overloadapp.ui.theme.MyGray40
-import com.example.overloadapp.ui.theme.MyGray80
-import com.example.overloadapp.ui.theme.OverloadAppTheme
-import android.Manifest
-import android.bluetooth.BluetoothDevice
-import android.content.BroadcastReceiver
-import android.content.IntentFilter
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.filled.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
+
 import com.example.overloadapp.ui.theme.MyBlue40
 import com.example.overloadapp.ui.theme.MyBlue80
 import com.example.overloadapp.ui.theme.MyGray40
 import com.example.overloadapp.ui.theme.MyGray80
 import com.example.overloadapp.ui.theme.OverloadAppTheme
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
-import android.bluetooth.BluetoothSocket
-import android.content.pm.PackageManager
-import android.os.Handler
-import android.os.Looper
-import android.widget.Toast
-import androidx.annotation.RequiresPermission
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.isActive
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+
 import java.io.IOException
 import java.io.InputStream
 import java.io.OutputStream
 import java.util.UUID
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+var bluetoothSocket: BluetoothSocket? = null
+var inputStream: InputStream? = null
+var outputStream: OutputStream? = null
+val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
 class RegisteringActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -90,10 +79,6 @@ class RegisteringActivity : ComponentActivity() {
     }
 }
 
-var bluetoothSocket: BluetoothSocket? = null
-var inputStream: InputStream? = null
-var outputStream: OutputStream? = null
-val MY_UUID: UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB")
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -141,9 +126,9 @@ fun BluetoothDeviceElement(
         verticalAlignment = Alignment.CenterVertically,
         modifier = modifier
             .fillMaxWidth() // Row가 가로로 꽉 차도록 설정
-            .height(64.dp)
-            .background(Color.White, shape = RoundedCornerShape(24.dp))
-            .padding(horizontal = 16.dp, vertical = 8.dp) // 좌우 패딩 추가
+            .height(72.dp)
+            .background(Color.White, shape = RoundedCornerShape(22.dp))
+            .padding(horizontal = 24.dp, vertical = 8.dp) // 좌우 패딩 추가
     ) {
         Text(
             text = name,
@@ -156,9 +141,9 @@ fun BluetoothDeviceElement(
         )
         Box(
             modifier = Modifier
-                .background(MyBlue40, shape = RoundedCornerShape(24.dp))
+                .background(MyBlue40, shape = RoundedCornerShape(14.dp))
                 .clickable { }
-                .padding(horizontal = 12.dp, vertical = 8.dp)
+                .padding(horizontal = 8.dp, vertical = 8.dp)
                 .clickable { onClick() }
         ) {
             Text(text = "선택", color = Color.White, fontWeight = FontWeight.Bold)
@@ -269,23 +254,17 @@ fun BluetoothStatusBox(innerPadding: PaddingValues) {
                         .fillMaxWidth()
                         .background(MyGray80) // 전체 바 배경 (필요없으면 제거)
                         .navigationBarsPadding()
+                        .verticalScroll(rememberScrollState())
                 ) {
                     pairedDevices.forEach { device -> // deviceNames 배열을 순회
                         BluetoothDeviceElement(
                             name = device.name, // 각 기기 이름을 전달
                             modifier = Modifier
-                                .padding(10.dp),
-                            onClick = { ConnectToDevice(context, device) }
+                                .padding(vertical = 5.dp, horizontal = 10.dp),
+                            onClick = { connectToDevice(context, device) }
                         )
                     }
                 }
-                /*
-                Text(
-                    text = "연결 가능한 기기 예시:\n" + deviceNames.joinToString("\n"),
-                    fontSize = 14.sp,
-                    color = Color.Black,
-                    textAlign = TextAlign.Center
-                )*/
             }
         }
     }
@@ -294,7 +273,7 @@ fun BluetoothStatusBox(innerPadding: PaddingValues) {
 
 
 @RequiresPermission(Manifest.permission.BLUETOOTH_CONNECT)
-fun ConnectToDevice(context: Context, device: BluetoothDevice) {
+fun connectToDevice(context: Context, device: BluetoothDevice) {
     Toast.makeText(context, "연결시도", Toast.LENGTH_SHORT).show()
 
     // 권한 재확인 (Android S 이상)
